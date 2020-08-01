93e14c8fa3e57b5329e826369909e575e8b8e9cd
Kyle Walters <kyle.p.walters@gmail.com>
2020-08-01 22:08:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   emoticon_lib = load_library(file_path)
-  emoticon = emoticon_lib.each do |key, value| 
+    emoticon_lib.each do |key, value| 
     value.each do |inner_key, faces|
       faces.each do |face|
         if face = emoticon
